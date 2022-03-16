9136063538df3fa76af644a6df8d75451bb99fd4
miZo <hamzaezzakri98@gmail.com>
2022-03-16 13:18:58 +0000

    project-structure

---
+++ src/main/java/com/crud/demo/repositories/ProduitRepository.java
@@ -0,0 +1,10 @@
+package com.crud.demo.repositories;
+
+import com.crud.demo.models.Produit;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProduitRepository extends JpaRepository<Produit,Long> {
+
+}
